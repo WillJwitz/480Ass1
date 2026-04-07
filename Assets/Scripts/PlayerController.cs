3ffcc5004802899f1808fd8348b993562f904218
WillJwitz <william.jurewitz12@gmail.com>
2026-04-06 17:19:12 -0700

    DONE! Pickups, score, win state, and proper double jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,31 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 
 public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
 
+    private bool grounded = true;
+    private bool dJump = true;
+
     public float speed = 0;
+    public float jumpForce = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();   
+        rb = GetComponent<Rigidbody>();
+        count = 0;   
+        winTextObject.SetActive(false);
+        SetCountText();
     }
 
     void OnMove(InputValue movementValue)
@@ -22,6 +33,23 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x; 
         movementY = movementVector.y;
+    }
+    //Implements jump and double jump
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {   
+            if(grounded)
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                grounded = false;
+            } else if (dJump)
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                dJump = false;
+            }
+            
+        }
     }
 
     void FixedUpdate()
@@ -31,4 +59,34 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+        
+    }
+
+    
+    // Double jump reset
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("floor"))
+        {
+            grounded = true;
+            dJump = true;
+        }
+    }
+
+    void SetCountText() 
+   {
+        countText.text =  "Count: " + count.ToString();
+        if (count >= 12)
+       {
+           winTextObject.SetActive(true);
+       }
+   }
 }
